367d15b47ef167ef14fa40a831cb981c1dc47a63
Steph Ma <1923220707@qq.com>
2024-06-14 16:38:17 +0800

    Hello, wolrd

--- Momo.java
+++ Momo.java
@@ -1,1 +1,6 @@
-
+public class Momo{
+    public static void main(String[]args){
+        System.out.println("Hello, Wolrd!");
+        return;
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Momo
-It's a little practice project.
-这是一个个人测试的小项目。
+- It's a little practice project.
+- 这是一个个人测试的小项目。
